b3a4657bb8b8d149bb4ba98974d6e3c0a8d4733d
Ashwini Kumar <ashkr03@gmail.com>
2026-09-13 14:16:46 +0530

    Fix CORS for cookie-based JWT auth

--- signinbh.php
+++ signinbh.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $allowedOrigins = ['http://localhost:5173', 'https://jwtbookhub.netlify.app'];
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 if (in_array($origin, $allowedOrigins)) {
